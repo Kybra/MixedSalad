88273616155e4f9154abf2dd9babda0021fb723a
Kübrâ <kuebraalbayrak@hotmail.de>
2025-03-08 22:07:07 +0100

    added comic book slider js

--- (Root-Verzeichnis)/script.js
+++ (Root-Verzeichnis)/script.js
@@ -1,1 +1,36 @@
-script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const pages = [
+        "comics/chapter-01/page-01.jpg",
+        "comics/chapter-01/page-02.jpg",
+        "comics/chapter-01/page-03.jpg"
+    ];
+
+    let currentPage = 0;
+
+    const comicPage = document.getElementById("comicPage");
+    const prevButton = document.getElementById("prevPage");
+    const nextButton = document.getElementById("nextPage");
+
+    function updateComic() {
+        comicPage.src = pages[currentPage];
+
+        prevButton.disabled = currentPage === 0;
+        nextButton.disabled = currentPage === pages.length - 1;
+    }
+
+    prevButton.addEventListener("click", function() {
+        if (currentPage > 0) {
+            currentPage--;
+            updateComic();
+        }
+    });
+
+    nextButton.addEventListener("click", function() {
+        if (currentPage < pages.length - 1) {
+            currentPage++;
+            updateComic();
+        }
+    });
+
+    updateComic(); // Set initial page
+});
